1c3054d9b2c544398461c55ce982e0819894a9b5
Wahyu Jebredz <wabredz1234@gmail.com>
2024-02-11 22:40:42 +0700

    second-commit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,4 +6,13 @@ app.get("/", (c) => {
   return c.text("Bismilah Sukses!");
 });
 
+app.notFound((c) => {
+  return c.text("Halaman tidak ditemukan...", 404);
+});
+
+app.onError((err, c) => {
+  console.error(`${err}`);
+  return c.text("Maaf ada yang error", 500);
+});
+
 export default app;
